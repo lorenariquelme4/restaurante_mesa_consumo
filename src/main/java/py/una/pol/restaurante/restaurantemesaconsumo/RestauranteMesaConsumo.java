aa2ab714bbfc0d2fdefeea31896166acd04a3fe7
fati026 <almadafati93@gmail.com>
2024-11-09 17:47:51 -0300

    Initial commit

---
+++ src/main/java/py/una/pol/restaurante/restaurantemesaconsumo/RestauranteMesaConsumo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package py.una.pol.restaurante.restaurantemesaconsumo;
+
+/**
+ *
+ * @author CAOE
+ */
+public class RestauranteMesaConsumo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
